7ac9aed10c0ed22af8e7e68ca235e5600191d60e
Ander <taguirone.at@gmail.com>
2024-07-12 22:52:03 -0300

    Atualizacoes

--- Desafio.py
+++ Desafio.py
@@ -1,121 +1,53 @@
-from time import sleep
+import Funções_Desafio
 
-
-def carregando(msg_1, msg_2):
-    print(msg_1)
-    print('.', end='')
-    sleep(1)
-    print('.', end='')
-    sleep(1)
-    print('.')
-    sleep(1)
-    print(msg_2)
-
-
-menu = """ 
--=-=-=-=-=-= BANCO =-=-=-=-=-=
-
-    [1] Deposito
-    [2] Saque
-    [3] Extrato
-    [4] Sair
-    
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-"""
+LIMITE_DE_SAQUES = 3
+AGENCIA = '0001'
 
 saldo = 0
 limite_por_saque = 500
-extrato = []
+extrato_conta = []
 numero_de_saques = 0
-LIMITE_DE_SAQUES = 3
-
+usuarios = []
+contas = []
 
 while True:
-
     try:
-        print(menu)
+        Funções_Desafio.menu()
         opcao = int(input('Escolha uma opção: '))
+
         if opcao == 1:
 
-                while True:
-                    try:
-                        valor_a_depositar = int(input('Quanto você deseja depositar?: '))
-
-                        if valor_a_depositar <= 0:
-                            print('Não é possivel depositar esse tipo de valor!\nTente um valor inteiro acima de 0!')
-
-                        else:
-                            saldo += valor_a_depositar
-                            extrato.append('Deposito')
-                            extrato.append(f'    + R${valor_a_depositar}')
-                            carregando('Fazendo deposito!', 'Valor depositado com sucesso!')
-                            break
-
-                    except ValueError:
-                        print('Valor digitado incorreto!, Aceitamos apenas valores inteiros!')
+            saldo, extrato_conta = Funções_Desafio.deposito(saldo, extrato_conta)
+            Funções_Desafio.carregando('Fazendo deposito!', 'Valor depositado com sucesso!')
 
         elif opcao == 2:
-            while True:
-                try:
-                    valor_a_sacar = int(input('Quanto você deseja sacar?: '))
-
-                    if saldo == 0:
-                        print('Não foi possivel realizar o saque!\nVocê não tem valores disponiveis para sacar!')
-                        print(f'Seu saldo atual é de R${saldo:.2f}')
-                        break
-
-                    elif valor_a_sacar > saldo:
-                        carregando('Fazendo saque!',
-                                   'Não foi possivel realizar o saque!\nO valor que deseja sacar e menor do que seu saldo!'
-                                   )
-                        print(f'Seu saldo atual é de R${saldo:.2f}')
-
-                    elif valor_a_sacar > limite_por_saque:
-                        print(f'Não foi possivel realizar o saque!\nO valor limite por saque é de R${limite_por_saque:.2f}!')
-
-                    elif LIMITE_DE_SAQUES == 0:
-                        carregando('Fazendo saque!', f'Não foi possivel realizar o saque!\nO limite de saque diario excedeu!')
-                        print('Tente novamente amanhã!')
-                        break
-
-                    else:
-                        saldo -= valor_a_sacar
-                        LIMITE_DE_SAQUES -= 1
-                        extrato.append('Saque')
-                        extrato.append(f'    - R${valor_a_sacar}')
-                        carregando('Fazendo saque!', 'Saque realizado com sucesso!')
-                        break
-
-                except ValueError:
-                    print('Valor digitado incorreto!, Aceitamos apenas valores inteiros!')
+            saldo, extrato_conta, LIMITE_DE_SAQUES = Funções_Desafio.saque(saldo=saldo, extrato_conta=extrato_conta, limite_de_saques=LIMITE_DE_SAQUES, limite_por_saque=limite_por_saque)
 
         elif opcao == 3:
-            carregando('Exibindo extrato', '')
-            print('=-=-=-=-=-= EXTRATO =-=-=-=-=-')
-            for i in range(0, len(extrato)):
-                print(f'{extrato[i]}')
-            print(f'Saldo: R${saldo:.2f}')
-            print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+            Funções_Desafio.extrato(extrato_conta, saldo=saldo)
 
         elif opcao == 4:
-            carregando('Saindo do sistema!', 'Obrigado por ultilizar nosso sistema, até a proxima :)')
+            cliente = Funções_Desafio.criar_usuario()
+
+            if not any(usuario.get('cpf') == cliente.get('cpf') for usuario in usuarios):
+                usuarios.append(cliente)
+                print('Usuário criado com sucesso!')
+            else:
+                print('Usuário já existe!')
+
+        elif opcao == 5:
+            Funções_Desafio.carregando('Saindo do sistema!', 'Obrigado por ultilizar nosso sistema, até a proxima :)')
             break
+
         else:
             print('Opção invalida, escolha uma das opções acima! ')
+            continue
 
-        verificacao = ''
-
-        while True:
-
-            verificacao = str(input(f'Deseja realizar outra operação? [S/N]: ')).upper().strip()
-
-            if verificacao not in 'SN':
-                print('Opção inválida! Digite S para Sim ou N para Não! ')
-            else:
-                break
+        verificacao = Funções_Desafio.verificacao_s_ou_n()
 
         if verificacao == 'N':
-            carregando('Saindo do sistema!', 'Obrigado por ultilizar nosso sistema, até a proxima :)')
+            Funções_Desafio.carregando('Saindo do sistema!', 'Obrigado por ultilizar nosso sistema, até a proxima :)')
             break
+
     except ValueError:
-        print('Valor digitado incorreto!\nEscolha uma das opções abaixo!')
+        print('Valor invalido, tente uma das opcoes abaixo!')
---
+++ Funções_Desafio.py
@@ -0,0 +1,123 @@
+def criar_usuario():
+    try:
+        nome = str(input('Digite seu nome: '))
+        data_nascimento = input('Digite sua data de nascimento (DD/MM/AAAA): ')
+        cpf = int(input('Digite seu CPF (Somente números): '))
+        print('Digite seu endereco! ')
+        logradouro = str(input('Logradouro: '))
+        numero = str(input('Numero da residencia: '))
+        bairro = str(input('Bairro: '))
+        cidade = str(input('Cidade / Sigla do Estado: '))
+        endereco = f'{logradouro}, {numero}, {bairro}, {cidade}'
+
+        cliente = {'nome': nome, 'data_nascimento': data_nascimento, 'cpf': cpf, 'endereco': endereco}
+
+        return cliente
+    except ValueError:
+        print('Valor inválido')
+
+
+def criar_conta():
+   pass
+
+
+def deposito(saldo, extrato_conta, /):
+    while True:
+        valor_a_depositar = int(input('Quanto você deseja depositar?: '))
+        try:
+
+            if valor_a_depositar <= 0:
+                print('Não é possivel depositar esse tipo de valor!\nTente um valor inteiro acima de 0!')
+
+            else:
+                saldo += valor_a_depositar
+                extrato_conta.append('Deposito')
+                extrato_conta.append(f'    + R${valor_a_depositar}')
+                return saldo, extrato_conta
+
+        except ValueError:
+            print('Valor digitado incorreto!, Aceitamos apenas valores inteiros!')
+
+
+def saque(*, saldo, extrato_conta, limite_de_saques, limite_por_saque):
+    while True:
+        try:
+            valor_a_sacar = int(input('Quanto você deseja sacar?: '))
+
+            if saldo == 0:
+                print('Não foi possivel realizar o saque!\nVocê não tem valores disponiveis para sacar!')
+                print(f'Seu saldo atual é de R${saldo:.2f}')
+                return saldo, extrato_conta, limite_de_saques
+
+            elif valor_a_sacar > saldo:
+                carregando('Fazendo saque!',
+                           'Não foi possivel realizar o saque!\nO valor que deseja sacar e menor do que seu saldo!'
+                           )
+                print(f'Seu saldo atual é de R${saldo:.2f}')
+
+            elif valor_a_sacar > limite_por_saque:
+                print(f'Não foi possivel realizar o saque!\nO valor limite por saque é de R${limite_por_saque:.2f}!')
+
+            elif limite_de_saques == 0:
+                carregando('Fazendo saque!', f'Não foi possivel realizar o saque!\nO limite de saque diario excedeu!')
+                print('Tente novamente amanhã!')
+                return saldo, extrato_conta, limite_de_saques
+
+            else:
+                saldo -= valor_a_sacar
+                limite_de_saques -= 1
+                extrato_conta.append('Saque')
+                extrato_conta.append(f'    - R${valor_a_sacar}')
+                carregando('Fazendo saque!', 'Saque realizado com sucesso!')
+                return saldo, extrato_conta, limite_de_saques
+
+        except ValueError:
+            print('Valor digitado incorreto!, Aceitamos apenas valores inteiros!')
+
+
+def extrato(extrato_conta, /, *, saldo):
+    carregando('Exibindo extrato', '')
+    print('=-=-=-=-=-= EXTRATO =-=-=-=-=-')
+    for i in range(0, len(extrato_conta)):
+        print(f'{extrato_conta[i]}')
+    print(f'Saldo: R${saldo:.2f}')
+    print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
+
+def carregando(msg_1, msg_2):
+    from time import sleep
+    print(msg_1)
+    print('.', end='')
+    sleep(1)
+    print('.', end='')
+    sleep(1)
+    print('.')
+    sleep(1)
+    print(msg_2)
+
+
+def verificacao_s_ou_n():
+    while True:
+
+        verificacao = str(input(f'Deseja realizar outra operação? [S/N]: ')).upper().strip()
+
+        if verificacao not in 'SN':
+            print('Opção inválida! Digite S para Sim ou N para Não! ')
+        else:
+            break
+    return verificacao
+
+
+def menu():
+    menu_banco = """ 
+    -=-=-=-=-=-= BANCO =-=-=-=-=-=
+
+        [1] Deposito
+        [2] Saque
+        [3] Extrato
+        [4] Criar Usuario
+        [5] Sair
+
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    """
+    print(menu_banco)
